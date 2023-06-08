a8b13ef9f80a61322fb6418690b011167aaece2e
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-08 08:36:30 +0600

    my classes page data load

--- index.js
+++ index.js
@@ -137,6 +137,12 @@ async function run() {
             const result = await classesCollection.insertOne(addClass);
             res.send(result);
         })
+        app.get('/classes/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await classesCollection.find(query).toArray();
+            res.send(result);
+        })
 
 
 
