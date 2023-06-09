1bef773da6140619a0a46e54fd5449edf6d34d1a
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-09 15:37:01 +0600

    enrolled class delete added

--- index.js
+++ index.js
@@ -250,6 +250,14 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/enrolledClasses/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await enrolledClassesCollection.deleteOne(query);
+            res.send(result);
+        })
+          
+
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
